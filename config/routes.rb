996df42baeb20df803bb9023e911bf05808d026a
Melanie <makes.melly@gmail.com>
2014-06-09 20:46:36 +0200

    Added sessions to help with login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'session#destroy', as: 'logout'
+
+
   resources :users
+  resources :sessions
 
   get 'welcome/index'
 
   get 'welcome/about'
 
 
-  root :to => 'welcome#index'
+  
 end
