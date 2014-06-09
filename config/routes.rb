77c573a1ae3a8ed5a80966754cc86a7531c5d5f7
Melanie <makes.melly@gmail.com>
2014-06-09 20:54:17 +0200

    changed signout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'session#destroy', as: 'logout'
-
 
   resources :users
   resources :sessions
