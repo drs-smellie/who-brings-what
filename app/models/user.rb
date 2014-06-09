f8e3ab28931f8ed6c7c90efdeaa63cebc0dadda3
Melanie <makes.melly@gmail.com>
2014-06-09 17:55:03 +0200

    creating users

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates_uniqueness_of   :email, :name
+  validates_presences_of    :email, :name
+
+  validates :password, length: { minimum: 6 }, allow_blank: false
+  validates :password, presence: true, on: :create
+
+end
