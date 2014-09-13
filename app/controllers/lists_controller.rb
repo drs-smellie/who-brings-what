85588d695592373426371465e4153c97ef0255c8
Melanie <makes.melly@gmail.com>
2014-09-13 21:07:03 +0200

    adjusting form for url

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,13 +7,17 @@ class ListsController < ApplicationController
   def create
     #TO DO How to get lists connected to an event
     @event = Event.find(params[:event_id])
-    @list = List.new
+    @list = current_user.lists.build(list_params)
+    @list.event = @event
+
     if @list.save
       redirect_to event_path(@event), notice: "Awesomesauce. You have created a list!"
     else
       render "new", notice: "Bummer! Something went wrong. Please try to save your list again."
     end
   end
+
+  
 
   private 
 
